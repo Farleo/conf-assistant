a501c1b22b9ba37cba7c5b11891cda03f7062e03
ivan <ikvuch@gmail.com>
2020-03-03 12:50:58 +0200

    Initial commit

---
+++ src/main/java/lms/itcluster/confassistant/ConfAssistantApplication.java
@@ -0,0 +1,13 @@
+package lms.itcluster.confassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfAssistantApplication.class, args);
+    }
+
+}
